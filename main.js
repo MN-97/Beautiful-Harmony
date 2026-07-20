f147154470c1a272aa25b4b7f8d37a3552ff8b5c
MN-97 <mana9419@icloud.com>
2026-07-20 14:42:08 +0900

    main.js を更新

--- main.js
+++ main.js
@@ -1,11 +1,27 @@
+const titleScreen = document.getElementById("titleScreen");
+const themeScreen = document.getElementById("themeScreen");
+const colorsScreen = document.getElementById("colorsScreen");
+
 const playButton = document.getElementById("playButton");
+const colorsButton = document.getElementById("colorsButton");
+const finishButton = document.getElementById("finishButton");
 
-const themeMenu = document.getElementById("themeMenu");
+playButton.addEventListener("click", () => {
 
-playButton.addEventListener("click",function(){
+    titleScreen.classList.add("hidden");
+    themeScreen.classList.remove("hidden");
 
-    playButton.style.display="none";
+});
 
-    themeMenu.classList.remove("hidden");
+colorsButton.addEventListener("click", () => {
+
+    themeScreen.classList.add("hidden");
+    colorsScreen.classList.remove("hidden");
+
+});
+
+finishButton.addEventListener("click", () => {
+
+    alert("Harmony 92%\n\n※採点画面は次回実装します。");
 
 });
